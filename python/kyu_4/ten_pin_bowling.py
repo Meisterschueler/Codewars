3f85a44906668e64f9df56a4d76850724dce8870
Meisterschueler <kammermark@gmx.de>
2021-10-28 08:41:43 +0200

    Update ten_pin_bowling.py

--- python/kyu_4/ten_pin_bowling.py
+++ python/kyu_4/ten_pin_bowling.py
@@ -1,25 +1,20 @@
 def bowling_score(frames):
+    # calculate scores of each single roll
     rolls = []
     for idx, frame in enumerate(frames.split(' ')):
+        last_round = idx == 9
         for roll in frame:
-            if roll.isdigit():
-                rolls.append(('d', int(roll), idx == 9))
-            elif roll == 'X':
-                rolls.append(('X', 10, idx == 9))
-            elif roll == '/':
-                rolls.append(('/', 10 - rolls[-1][1], idx == 9))
+            scores = 10 - rolls[-1][1] if roll == '/' else 10 if roll == 'X' else int(roll)
+            rolls.append((roll, scores, last_round))
     
+    # calculate total scores with boni because of spares (/) and strikes (X)
     total_scores = 0
-    for idx, (command, scores, last_round) in enumerate(rolls):
-        if command == 'd':
-            total_scores += scores
-        elif command == 'X':
-            total_scores += 10
-            if not last_round:
-                total_scores += rolls[idx+1][1] + rolls[idx+2][1]
-        elif command == '/':
-            total_scores += scores
-            if not last_round:
+    for idx, (roll, scores, last_round) in enumerate(rolls):
+        total_scores += scores
+        if not last_round:
+            if roll == '/':
                 total_scores += rolls[idx+1][1]
+            elif roll == 'X':
+                total_scores += rolls[idx+1][1] + rolls[idx+2][1]
 
     return total_scores
